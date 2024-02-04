f3b020a3d3f02c21b91c1b30d63d587d0c7976bf
Shimamura Sakura <shimamura_sakura@pm.me>
2024-02-04 19:11:24 +0800

    linksaver: import/export

--- linksaver/index.js
+++ linksaver/index.js
@@ -13,7 +13,7 @@ document.getElementById('btn_tryparse').addEventListener('click', function () {
         const tag = plain.split('\n');
         const link = plain.match(/weibo.com\/u\/\d+/);
         if (tag.length >= 2 && link)
-            return fillForm(link[0], tag[0]);
+            return parseFill(link[0], tag[0]);
     }
     /*
      * 轻松不上头的个人空间-分享自哔哩哔哩 https://b23.tv/sU7d4rE
@@ -25,7 +25,7 @@ document.getElementById('btn_tryparse').addEventListener('click', function () {
         const link = plain.match(/b23\.tv\/.+/);
         const tryReal = plain.match(/space\.bilibili\.com\/\d+/);
         if (tag.length >= 2 && tryReal)
-            return fillForm(tryReal[0], tag[0]);
+            return parseFill(tryReal[0], tag[0]);
         if (tag.length >= 2 && link) {
             const xhr = new XMLHttpRequest();
             xhr.open('GET', `https://${link[0]}`, true);
@@ -33,9 +33,9 @@ document.getElementById('btn_tryparse').addEventListener('click', function () {
                 if (xhr.readyState == xhr.DONE) {
                     const url = new URL(xhr.responseURL);
                     if (url.hostname == 'm.bilibili.com' && url.pathname.startsWith('/space/'))
-                        return fillForm(`space.bilibili.com/${url.pathname.replace('/space/', '')}`, tag[0]);
+                        return parseFill(`space.bilibili.com/${url.pathname.replace('/space/', '')}`, tag[0]);
                     if (url.hostname == 'space.bilibili.com')
-                        return fillForm(`${url.hostname}${url.pathname}`, tag[0]);
+                        return parseFill(`${url.hostname}${url.pathname}`, tag[0]);
                     alert('should be unreachable');
                 }
             };
@@ -54,6 +54,11 @@ document.getElementById('btn_formclear').addEventListener('click', function () {
     e_inputtags.value = '';
 });
 const e_tablesave = document.getElementById('table_saved');
+function parseFill(link, tag) {
+    if (!(link.startsWith('http://') || link.startsWith('https://')))
+        link = 'https://' + link;
+    return fillForm(link, tag);
+}
 function fillForm(link, tag) {
     e_inputlink.value = link;
     e_inputtags.value = tag;
@@ -84,19 +89,49 @@ function loadFromStorage() {
 function saveToStorage() {
     localStorage.setItem('linkSaver', JSON.stringify(g_linkData));
 }
-document.getElementById('btn_save').addEventListener('click', function () {
-    let link = e_inputlink.value;
-    const tags = e_inputtags.value.replace(/[ \r\n\t\f\v]/g, '').split(',');
+function gAddEntry(link, tags) {
     if (link.length == 0 || tags.length == 0) return;
-    if (!(link.startsWith('http://') || link.startsWith('https://')))
-        link = 'https://' + link;
     if (link in g_linkData)
         for (const oldtag of g_linkData[link])
             if (tags.indexOf(oldtag) == -1)
                 tags.push(oldtag);
     g_linkData[link] = tags.sort();
+}
+document.getElementById('btn_save').addEventListener('click', function () {
+    let link = e_inputlink.value;
+    const tags = e_inputtags.value.replace(/[ \r\n\t\f\v]/g, '').split(',');
+    gAddEntry(link, tags);
     saveToStorage();
     recreateTableItems();
 });
 loadFromStorage();
 recreateTableItems();
+
+const e_inputimport = document.getElementById('input_import');
+const e_aexport = document.getElementById('a_export');
+document.getElementById('btn_import').onclick = () => e_inputimport.click();
+e_inputimport.addEventListener('change', async function () {
+    const file = e_inputimport.files[0];
+    if (!file.name.toLowerCase().endsWith('.json')) alert('select a json');
+    try {
+        const data = JSON.parse(await file.text());
+        if (typeof data != 'object') throw new Error('data not object');
+        for (const key in data) {
+            if (!Array.isArray(data[key])) throw new Error('value not array');
+            if (!data[key].every(t => typeof t == 'string')) throw new Error('array not of string');
+            gAddEntry(key, data[key]);
+        }
+        saveToStorage();
+        recreateTableItems();
+    } catch (e) {
+        alert(e.message);
+    }
+});
+let lastExportURL = null;
+document.getElementById('btn_export').addEventListener('click', function () {
+    if (lastExportURL) URL.revokeObjectURL(lastExportURL);
+    lastExportURL = URL.createObjectURL(new Blob([JSON.stringify(g_linkData)], { type: 'application/json' }));
+    e_aexport.href = lastExportURL;
+    e_aexport.download = `linksaver-${Date.now()}.json`;
+    e_aexport.click();
+});
