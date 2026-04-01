d3ce45d02ca5a7e38c3c628392a1f589766850cd
lipsum <shimamura_sakura@pm.me>
2026-04-01 15:21:42 +0800

    asar: make chrome happy (incr timeout to 5s)

--- sw.js
+++ sw.js
@@ -4,6 +4,8 @@ function waitTime(t) {
   return new Promise(resolve => setTimeout(resolve, t));
 }
 
+const msgHandlers = {};
+self.addEventListener('message', ev => Object.values(msgHandlers).forEach(f => f(ev)));
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('active', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => {
@@ -11,19 +13,19 @@ self.addEventListener('fetch', e => {
   e.respondWith((async () => {
     const client = await self.clients.get(e.clientId);
     if (!client) return fetch(e.request);
-    const uuid = crypto.randomUUID();
+    const uuid = crypto.randomUUID(), mUUID = crypto.randomUUID();
     const url = decodeURI(new URL(e.request.url).pathname).toLowerCase();
     const { promise, resolve } = Promise.withResolvers();
     function handleMsg(ev) { if (ev.data.uuid == uuid) resolve(ev.data.reply); }
-    self.addEventListener('message', handleMsg);
     try {
+      msgHandlers[mUUID] = handleMsg;
       client.postMessage({ uuid, url });
-      const res = await Promise.race([promise, waitTime(250)]);
+      const res = await Promise.race([promise, waitTime(5000)]);
       return res ? new Response(res) : fetch(e.request);
     } catch (e) {
       return fetch(e.request);
     } finally {
-      self.removeEventListener('message', handleMsg);
+      delete msgHandlers[mUUID];
     }
   })());
 });
